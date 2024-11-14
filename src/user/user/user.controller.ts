800e0e8a8ab59d27f69a1e411a85ea5d6f5eeaa7
siwakasen <siwakasen@gmail.com>
2024-11-15 02:05:53 +0700

    update user controller

--- src/user/user/user.controller.ts
+++ src/user/user/user.controller.ts
@@ -1,5 +1,16 @@
-import { Controller, Ip, HostParam, Query, Param } from '@nestjs/common';
-import { Get, Post } from '@nestjs/common';
+import {
+  Controller,
+  Ip,
+  HostParam,
+  Query,
+  Param,
+  Get,
+  Post,
+  Header,
+  HttpCode,
+  HttpRedirectResponse,
+  Redirect,
+} from '@nestjs/common';
 
 @Controller('/api/users')
 export class UserController {
@@ -15,9 +26,33 @@ export class UserController {
   }
 
   @Get('/hello')
-  hello(@Query('name') name: string, @Query('age') age: number): string {
+  async hello(
+    @Query('name') name: string,
+    @Query('age') age: number,
+  ): Promise<string> {
     return `Hello ${name}! You are ${age} years old.`;
   }
+
+  @Get('/sample')
+  @Header('Content-Type', 'application/json')
+  @HttpCode(200)
+  sampleResponse(): Record<string, string> {
+    return {
+      message: 'This is a sample response',
+      status: 'success',
+      data: 'This is a sample data',
+    };
+  }
+
+  @Get('/redirect')
+  @Redirect()
+  redirect(): HttpRedirectResponse {
+    return {
+      statusCode: 302,
+      url: '/api/users/sample',
+    };
+  }
+
   @Get('/:id')
   getUserById(@Param('id') id: string): string {
     return `Get user with id ${id}`;
